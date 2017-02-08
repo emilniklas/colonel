5d4a2d2ed0e3cafc80942ddeed85998c59fc4784
Emil Persson <emil.persson@creuna.se>
2017-02-08 01:36:58 +0100

    Add exports to index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export {Program} from './Program'
+export {Command} from './Command'
+
+export {Handler} from './Handler/Handler'
+export {CommandHandler} from './Handler/CommandHandler'
+
+export {OptionsProvider} from './Options/OptionsProvider'
+export {DefaultsOptionsProvider} from './Options/DefaultsOptionsProvider'
+
+export {Awaitable} from './Util/Awaitable'
+export {Partial} from './Util/Partial'
